70212f899a179b0a61c38af754bee6ea0779de16
eee685 <christopher.ng2@student.education.wa.edu.au>
2025-03-23 19:49:47 +0800

    Update level1.py
    level1.py changed to have pushable and enemy work, but pushable doesn't work right, see pushable class' changes. also, the enemy vibrates when colliding with a sprite, probably because it keeps moving towards player on the other side and the enemy position keeps reseting

--- level1.py
+++ level1.py
@@ -10,9 +10,28 @@ from wall_class import wall
 #import player class
 from player_class import player
 
+#import pushable class
+from pushable_block_class import pushable
+
+#import enemy class
+from enemy_class import enemy
+
+#create list of enemies
+enemies = [
+    enemy(700, 700, 40, 40, 3)  #enemy 1
+]
+
 #create wall objects and store in list
 walls = [
     wall(400, 200, 100, 50)  #test wall; x_axis, y_axis, length, width
+]
+
+#list of pushable blocks
+pushable_blocks = [
+    pushable(100, 100, 100, 100, 1),  #test block 1; x_axis, y_axis, length, width, speed
+    pushable(200, 200, 50, 50, 1),  #test block 2
+    pushable(300, 300, 20, 20, 1),   #test block 3
+    pushable(400, 400, 10, 10, 1)   #test block 3
 ]
 
 #create player object
@@ -33,6 +52,7 @@ while running:
 
     screen.fill((200, 255, 200))  #make the screen RGB COLOUR, is green for now, is in main loop to make screen keep reloading to stop the player from leaving a trail on the screen
 
+    #PLAYER SPRITE STUFF
     #bring walls list to player_class when drawing the player
     player_instance.draw(screen, walls=walls)
 
@@ -44,9 +64,41 @@ while running:
         else:
             print("no collision")
 
+    #STUFF FOR THE PUSHABLE BLOCKS
+    #checks for keys getting pressed
+    keys = pygame.key.get_pressed()
+    direction = (0, 0)  #default direction of block
+    if keys[pygame.K_UP]:
+        direction = (0, -1) #moving up to 0
+    elif keys[pygame.K_DOWN]:
+        direction = (0, 1) #moving down to max number
+    elif keys[pygame.K_LEFT]:
+        direction = (-1, 0) #moving left to 0
+    elif keys[pygame.K_RIGHT]:
+        direction = (1, 0) #moving right to max number
+
+    #collisions and movement for pushable blocks
+    for block in pushable_blocks:
+        #gets the list of blocks, then excludes the block so the original block doesn't collide with itself
+        other_sprites = [b for b in pushable_blocks if b != block] + walls
+        if block.check_for_collision(player_instance):  #if player collides with block
+            print("got block collision")
+            block.push(direction, sprites=[player_instance] + other_sprites)  #push the block
+        block.draw(screen)  #draw blocks
+
+        #ENEMY STUFF
+    for enemy_instance in enemies:
+        enemy_instance.draw(screen, player_instance, walls, pushable_blocks)  #move and draw enemy
+        #check for collision with player sprite, if colliding with player then:
+        if enemy_instance.check_for_collision(player_instance):
+            print("game over")#need to add code to send player back to menu
+    #for debugging
+    print(f"Player position: {player_instance.x_position}, {player_instance.y_position}")
+    print(f"Block position after: {block.x_position}, {block.y_position}")
+    print(f"Direction: {direction}")
+    
     #update display
     pygame.display.flip()
 
     clock.tick(60)  #limit frame rate to 60 fps
 pygame.quit()  #quit pygame
-
