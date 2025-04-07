4789ebab7784eddb32cb9714d33a040b0aa08fff
eee685 <christopher.ng2@student.education.wa.edu.au>
2025-04-07 09:39:32 +0800

    Update pushable_block_class.py
    fixed pushable by moving the new position calculation into the collision function

--- pushable_block_class.py
+++ pushable_block_class.py
@@ -15,37 +15,37 @@ class pushable(wall):
 
     def push(self, direction, sprites):#block pushing method
         #to see if block has really been pushed
-        print("block has been pushed, push method works")
-        print(f"block position before push: {self.x_position}, {self.y_position}")
+        #print("block has been pushed, push method works")
+        #print(f"block position before push: {self.x_position}, {self.y_position}")
         x_move, y_move = direction
         # direction, x_move and y_move is change in block position, e.g. x_move=1, then it moves by block_speed amount of pixels one time, negative is reverse direction
         self.is_pushed = False#reset pushed boolean
-
+        
         #save original position
         original_x = self.x_position
         original_y = self.y_position
         #temporarily move block, block_speed amount of pixels moved by x_move or y_move amount
-        self.x_position += x_move * self.__block_speed
-        self.y_position += y_move * self.__block_speed
         #block position after push for debugging
-        print(f"block position after push and before collision check: {self.x_position}, {self.y_position}")
-
+        #print(f"block position after push and before collision check: {self.x_position}, {self.y_position}")
+        
         #check for collisions with sprites
         collision = False
         for sprite in sprites:
             if self.check_for_collision(sprite):
                 print(f"Collision detected with {sprite}")
-                #revert to old position
-                self.x_position = original_x
-                self.y_position = original_y
-                break #exit the loop on first collision
+                #change position after collision
+                self.x_position += x_move * self.__block_speed
+                self.y_position += y_move * self.__block_speed
+                print(f"This is x_position: {self.x_position}")
+                print(f"This is y_position: {self.y_position}")
+                #exit the loop on first collision
         #if no collisions happened, block was successfully pushed
         if not collision:
             self.is_pushed = True
         #for debigging
-        print(f"block position after push and after collision check): {self.x_position}, {self.y_position}")
-        print(f"Block speed: {self.__block_speed}")#to see if block speed is changing from 0
-        print(f"Sprites: {sprites}")#to see if it is loading sprites to collides
+        #print(f"block position after push and after collision check): {self.x_position}, {self.y_position}")
+        #print(f"Block speed: {self.__block_speed}")#to see if block speed is changing from 0
+        #print(f"Sprites: {sprites}")#to see if it is loading sprites to collides
 
 
     def check_for_collision(self, other):
@@ -54,5 +54,5 @@ class pushable(wall):
 
     def draw(self, screen, color=(0, 0, 255)):
         #draw block on screen
-        print(f"block position: {self.x_position}, {self.y_position}") #to see if the block is moving position just not drawing in new position
+        #print(f"block position: {self.x_position}, {self.y_position}") #to see if the block is drawn properly after moving position 
         pygame.draw.rect(screen, color, pygame.Rect(self.x_position, self.y_position, self.width, self.length))
